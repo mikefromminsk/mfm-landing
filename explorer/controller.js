4ff545a365102812e23f0dce77bccb18015d498d
Mike Haiduk <x29a100@gmail.com>
2025-09-06 16:19:40 +0300

    init

---
+++ explorer/controller.js
@@ -0,0 +1,69 @@
+function start($scope) {
+    addScopeUtils($scope)
+
+    $scope.multiSearch = function () {
+        if ($scope.search_text.length == 64) {
+            $scope.trans = null
+            loadTran($scope.search_text)
+        } else if ($scope.search_text.startsWith('V')) {
+            $scope.tran = null
+            loadTrans()
+            loadAccounts()
+        }
+    }
+
+    function loadTrans() {
+        post("mfm-token/trans", {
+            address: $scope.search_text,
+            size: 100,
+        }, function (response) {
+            $scope.trans = $scope.groupByTimePeriod(response.trans)
+            $scope.$apply()
+        })
+    }
+
+    function loadAccounts() {
+        post("mfm-token/accounts", {
+            address: $scope.search_text,
+        }, function (response){
+            $scope.accounts = response.accounts
+            $scope.$apply()
+        })
+    }
+
+    $scope.openTran = function (tran) {
+        console.log(tran)
+    }
+
+    function loadTran(hash) {
+        post("mfm-token/tran", {
+            next_hash: hash,
+        }, function (response){
+            $scope.tran = response.tran
+            loadTokenProfile(response.tran.domain)
+            $scope.$apply()
+        })
+    }
+
+    function loadTokenProfile(domain) {
+        post("mfm-token/token", {
+            domain: domain
+        }, function (response) {
+            $scope.token = response.token
+            $scope.$apply()
+        })
+    }
+
+    $scope.loadVersion = function () {
+        post("mfm-token/version", {}, function (response) {
+            $scope.node = response
+            $scope.$apply()
+        })
+    }
+
+    if (getParam("hash") != null) {
+        $scope.search_text = getParam("hash")
+        $scope.multiSearch()
+    }
+    $scope.loadVersion()
+}
